2c3377cc3481d844eee722d094851c3b4b59133e
omorsultan <omorsultansust@gmail.com>
2025-07-02 10:33:05 +0600

    False position

---
+++ NMAL-02/false_position.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double f(double x) {
+    return x*x*x - x - 2; 
+}
+
+pair<double, int> falsePosition(double a, double b, double tol) {
+    int iteration = 0;
+    double c;
+
+    if (f(a) * f(b) > 0) {
+        cout << "Invalid interval: f(a) and f(b) must have opposite signs.\n";
+        return {-1, 0};
+    }
+
+    cout << "Iter\t a \t\t b \t\t c \t\t f(c) \n";
+    while (true) {
+        c = b - (f(b) * (a - b)) / (f(a) - f(b));
+        double fc = f(c);
+        iteration++;
+
+        cout << iteration << "\t" << fixed << setprecision(6) << a << "\t" << b << "\t" << c << "\t" << fc << "\n";
+
+        if (fabs(fc) < tol)
+            break;
+
+        if (f(a) * fc < 0)
+            b = c;
+        else
+            a = c;
+    }
+
+    return {c, iteration};
+}
+
+int main() {
+    double a = 1, b = 2 , tolerance = 1e-6;       
+
+    auto result = falsePosition(a, b, tolerance);
+
+    cout << "\nApproximate Root = " << result.first << endl;
+    cout << "Total Iterations = " << result.second << endl;
+
+    return 0;
+}
